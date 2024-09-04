8b8349fed00e4aaf29622e04b1ad8b516847df53
jkw301 <julien.khalifa.95@gmail.com>
2024-09-04 20:26:10 +0200

    new folder

--- img tmp/img_to_png.py
+++ img tmp/img_to_png.py
@@ -1,20 +1,14 @@
 import os
 from PIL import Image
 
-# Chemin du répertoire parent
 directory = '../'
 
-# Parcourir tous les fichiers du répertoire parent
 for filename in os.listdir(directory):
     if filename.endswith('.img'):
-        # Chemin complet du fichier .img
         img_path = os.path.join(directory, filename)
         
-        # Ouvrir le fichier .img
         with Image.open(img_path) as img:
-            # Chemin complet pour le fichier .png
             png_path = os.path.splitext(img_path)[0] + '.png'
             
-            # Sauvegarder l'image en .png
             img.save(png_path)
             print(f'Converti : {filename} en {os.path.basename(png_path)}')
